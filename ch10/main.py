95bddf78869de4a41c16f136ef0ea5e649b221a0
mjennings754 <mitchellj@outlook.jp>
2026-01-05 14:24:03 +1100

    added some more examples

--- ch10/main.py
+++ ch10/main.py
@@ -86,6 +86,7 @@ class Person():
 
 class EmailPerson(Person):
     def __init__(self, name, email):
+        # super() will get Person() definition
         super().__init__(name)
         self.email = email
 
@@ -138,3 +139,39 @@ class Duck:
 
 fowl = Duck('Daffy')
 print(fowl.name)
+
+# Getter and Setters
+
+class Duck():
+    def __init__(self, input_name):
+        self.hidden_name = input_name
+    def get_name(self):
+        print('inside the getter')
+        return self.hidden_name
+    def set_name(self, input_name):
+        print('inside the setter')
+        self.hidden_name = input_name
+
+don = Duck('Donald')
+print(don.get_name())
+don.set_name('Donna')
+print(don.get_name())
+
+# Properties for Attribute Access
+
+class Duck():
+    def __init__(self, input_name):
+        self.hidden_name = input_name
+    def get_name(self):
+        print('inside the getter')
+        return self.hidden_name
+    def set_name(self, input_name):
+        print('inside the setter')
+        self.hidden_name = input_name
+    name = property(get_name, set_name)
+
+don = Duck('Donald')
+print(don.name)
+don.name = 'Donna'
+print(don.name)
+
