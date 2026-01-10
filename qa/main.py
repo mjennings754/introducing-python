24660dfa6f6eb88ecb1862c0235a012f08ada1f7
mjennings754 <mitchellj@outlook.jp>
2026-01-10 13:54:27 +1100

    add another question

--- qa/main.py
+++ qa/main.py
@@ -3,7 +3,8 @@ questions = ["What is Python, can you explain it?", "What is lambda?", "What is 
              "What are objects?", "How do you create a function?", "What is () within a function definition?", "What is *args, **kwargs", "What is the use of a docstring?", "What are inner functions used for?"
              "What is a generator?", "What are decorators?", "What is Dunder?", "How to initialize within a class?",
              "How do you inherit from a parent class?", "What does super() do?", "What are instance methods", "What are class methods",
-             "What are static methods?", "What is Duck Typing?", "What is a named tuple?", "What are Dataclasses?", "How to use a module inside a .py file?"]
+             "What are static methods?", "What is Duck Typing?", "What is a named tuple?", "What are Dataclasses?", "How to use a module inside a .py file?", "How do you reverse a string?", "What is a simple way to get pi?",
+             ]
 
 for question in questions:
     print(question)
