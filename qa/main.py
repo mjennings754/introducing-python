ffd776e73923092ea1847a3111128c2fd7ae2089
mjennings754 <mitchellj@outlook.jp>
2026-01-10 13:52:59 +1100

    add questions for self exam

---
+++ qa/main.py
@@ -0,0 +1,9 @@
+questions = ["What is Python, can you explain it?", "What is lambda?", "What is tuple unpacking?", "How do you create a dictionary?", "What do you use to create an immutable set?",
+             "How do you create a tuple?", "What is list comprehension?", "How do you change an item inside a Dictionary?", "How do you combine dictionaries?", "What are functions?",
+             "What are objects?", "How do you create a function?", "What is () within a function definition?", "What is *args, **kwargs", "What is the use of a docstring?", "What are inner functions used for?"
+             "What is a generator?", "What are decorators?", "What is Dunder?", "How to initialize within a class?",
+             "How do you inherit from a parent class?", "What does super() do?", "What are instance methods", "What are class methods",
+             "What are static methods?", "What is Duck Typing?", "What is a named tuple?", "What are Dataclasses?", "How to use a module inside a .py file?"]
+
+for question in questions:
+    print(question)
