85ddba87a7ebf387f12b25e8ddb9cc0464c80aee
mjennings754 <mitchellj@outlook.jp>
2026-01-11 17:08:40 +1100

    add more examples

--- ch15/main.py
+++ ch15/main.py
@@ -1,3 +1,18 @@
 import os
 os.getpid()
 os.getcwd()
+
+# User & Group ID
+os.getuid()
+os.getgid()
+
+# Unix date program
+import subprocess
+ret = subprocess.getoutput('date')
+ret
+
+import os
+os.uname()
+os.getloadavg()
+os.cpu_count()
+
---
+++ ch15/mp2.py
@@ -0,0 +1,23 @@
+import multiprocessing
+import os
+import time
+
+def whoami(name):
+           print("Process %s says: %s" % (name, os.getpid()))
+
+def loopy(name):
+        whoami(name)
+        start = 1
+        stop = 1000000
+        for num in range(start, stop):
+                print("\tNumber %s of %s. Honk!" % (num, stop))
+                time.sleep(1)
+if __name__ == "__main__":
+        whoami("main")
+        p = multiprocessing.Process(target=loopy, args=("loopy",))
+        p.start()
+        time.sleep(5)
+        p.terminate()
+
+
+
